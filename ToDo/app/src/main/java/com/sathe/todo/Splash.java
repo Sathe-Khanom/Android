9a72ae334456c20df486870363ccfc701b6f45c3
Sathe-Khanom <sathekhanom550@gmail.com>
2025-09-28 15:44:13 +0600

    add

--- ToDo/app/src/main/java/com/sathe/todo/Splash.java
+++ ToDo/app/src/main/java/com/sathe/todo/Splash.java
@@ -15,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class Splash extends AppCompatActivity {
 
     private TextView splashText;
-    private String massage=" This is the first step to organizing your day !";
+    private String massage="Let's get your life organized !";
     private int index=0;
     private long delay=150;//delay in ms per charecter
 
